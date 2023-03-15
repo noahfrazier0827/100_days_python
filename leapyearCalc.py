4703422986d00fb06094d95308deff98f0140a7f
noahfrazier0827 <noahfrazier1999@gmailcom>
2023-03-15 18:14:42 -0400

    updatded the code

--- leapyearCalc.py
+++ leapyearCalc.py
@@ -4,8 +4,9 @@ yearA = year % 4
 yearB = year % 100
 yearC = year % 400
 
-if yearA == 0 and yearB > 0:
-    yearC == 0
+if yearC == 0:
+    print("Leap year.")
+elif yearC > 0 and yearA == 0:
     print("Leap year.")
 else:
     print("Not leap year.")
