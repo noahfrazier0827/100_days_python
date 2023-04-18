24bc738eed67f61e950145d2320681214db8e061
noahfrazier0827 <noahfrazier1999@gmailcom>
2023-04-18 16:08:24 -0400

    update

--- travelLog.py
+++ travelLog.py
@@ -11,9 +11,12 @@ travel_log = [
 },
 ]
 
-add_new_country = travel_log[1]
-
-
+def add_new_country(country, visits, cities):
+    new_entry = {}
+    new_entry["country"] = country
+    new_entry["visits"] = visits
+    new_entry["cities"] = cities
+    travel_log.append(new_entry)
 
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
